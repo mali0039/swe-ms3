6863892aed50b47bd92e4dca8ce467b3b0c88428
Mustafa Ali <mali39@student.gsu.edu>
2022-02-03 13:10:47 -0500

    Added error page

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = flask.Flask(__name__)
 @app.route("/")  # Python decorator
 def main():
     movieDetails = getRandomMovie()
-    print(movieDetails)
     return flask.render_template("index.html", data=movieDetails)
 
 app.run(
