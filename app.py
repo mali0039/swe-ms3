8c7a9b23bf4d9645132a7d6c85a309560d239ad7
Mustafa Ali <mali39@student.gsu.edu>
2022-02-04 11:58:00 -0500

    Fixed Heroku configurations

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from tmbd import getRandomMovie
+import os
 app = flask.Flask(__name__)
 from wiki import getWikiURL
 @app.route("/")  # Python decorator
@@ -10,5 +11,6 @@ def main():
     return flask.render_template("index.html", data=movieDetails, id=id)
 
 app.run(
-    debug=True
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
 )
