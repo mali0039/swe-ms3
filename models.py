cedc5cbed0d3cf43f1ed2a677ba7cd00003f8ed5
Mustafa Ali <mali39@student.gsu.edu>
2022-02-24 13:05:18 -0500

    Created comments and user models

---
+++ models.py
@@ -0,0 +1,31 @@
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+db = SQLAlchemy()
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(24), nullable=False)
+    movieID = db.Column(db.String(10), nullable=False)
+    text = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Comment %r>' % self.text
+
+    def __init__(self, name, movieID, text, rating):
+        self.username = name
+        self.movieID = movieID
+        self.text = text
+        self.rating = rating
+        
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(24), nullable=False)
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+    def __init__(self, name):
+        self.username = name
+
